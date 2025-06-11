f0adf40f53c6598d9c1900e8ded474058a7d10f9
aishwarya <ashp17star@gmail.com>
2025-06-11 16:28:37 +0530

    .

---
+++ 23_04_25_TableView_Demo_2/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  23_04_25_TableView_Demo_2
+//
+//  Created by Vishal Jagtap on 07/06/25.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
